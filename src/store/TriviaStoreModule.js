f40631f002ba4611700e9e4af0769b29e6b54224
Vuk Kapor <vukkapor12@gmail.com>
2019-07-19 18:05:09 +0200

    vuex

---
+++ src/store/TriviaStoreModule.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const TriviaStoreModule = {
+  namespaced: true,
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+
+  }
+}
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import {
+  ChuckStoreModule
+} from './ChuckStoreModule'
+import {
+  TriviaStoreModule
+} from './TriviaStoreModule'
 
 Vue.use(Vuex)
 
 /* eslint-disable no-new */
 const store = new Vuex.Store({
   modules: {
+    ChuckStoreModule,
+    TriviaStoreModule
   }
 })
 
